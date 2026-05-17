beb166aa5ea0576cfc54cbd779c914e79d52f507
Ming Hong <tangmh17@gmail.com>
2026-05-17 11:28:54 +0800

    Add backend/app/schemas/transaction.py

---
+++ backend/app/schemas/transaction.py
@@ -0,0 +1,24 @@
+from datetime import date, time
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+from typing import List, Literal
+from uuid import UUID
+
+TransactionRange = Literal["1D", "1M", "1Y"]
+
+class TransactionResponse(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel, 
+        populate_by_name=True,
+        from_attributes=True
+    )
+
+    id: UUID
+    date: date
+    time: time
+    description: str
+    category: str
+    account: str
+    amount: float
+    impact: str
+    range: List[TransactionRange]
