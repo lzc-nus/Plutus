c6f5cb1576e0669b97d0459e1e89773ed39919f4
Ming Hong <tangmh17@gmail.com>
2026-05-23 15:35:43 +0800

    Add backend/app/routers/__init__.py

---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,5 @@
+from .user import router as user_router
+from .portfolio import router as portfolio_router
+from .goals import router as goals_router
+from .transactions import router as transactions_router
+from .insights import router as insights_router
