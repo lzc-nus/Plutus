d0b69df2877c350fb61b17cab9bca3b4a3a7f11f
Ming Hong <tangmh17@gmail.com>
2026-05-25 22:07:56 +0800

    feat: add TransactionUpdate to backend/app/schemas/transaction.py

--- backend/app/schemas/transaction.py
+++ backend/app/schemas/transaction.py
@@ -1,7 +1,7 @@
 from datetime import date, time
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
-from typing import List, Literal
+from typing import List, Literal, Optional
 from uuid import UUID
 
 TransactionRange = Literal["1D", "1M", "1Y"]
@@ -37,3 +37,13 @@ class TransactionResponse(BaseModel):
     amount: float
     impact: str
     range: List[TransactionRange]
+
+class TransactionUpdate(BaseModel):
+    date: Optional[date] = None
+    time: Optional[time] = None
+    description: Optional[str] = None
+    category: Optional[str] = None
+    account: Optional[str] = None
+    amount: Optional[float] = None
+    impact: Optional[str] = None
+    range: Optional[list[str]] = None
