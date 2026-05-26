044d55738ecd9307d12251cd07f0a23a6932d96f
lzc-nus <e1528600@u.nus.edu>
2026-05-27 03:14:17 +0800

    Refactor the whole frontend structure

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,21 +1,15 @@
 'use client';
 
 import React, { useState } from 'react';
-import PasswordValidator from '@/components/PasswordValidator';
+import PasswordValidator from '@/components/auth/PasswordValidator';
 
 export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState(false);
     
     const handleSignUp = async (e: React.BaseSyntheticEvent) => {
         e.preventDefault();
-        setError('');
-
-        try {
-            const res = await fetch('http://127.0.0.1:8000/api/auth/register')
-        }
+        
     }
 }
