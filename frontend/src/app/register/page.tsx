6b62eec9774293cd3b59df28f71aa90316101aef
Ming Hong <tangmh17@gmail.com>
2026-05-26 20:54:36 +0800

    Add frontend/src/app/register/page.tsx

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useState } from 'react';
+import PasswordValidator from '@/components/PasswordValidator';
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSignUp = async (e: React.BaseSyntheticEvent) => {
+        e.preventDefault();
+        
+    }
+}
