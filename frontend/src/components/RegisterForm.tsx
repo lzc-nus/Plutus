d2c6671f21afc8a0b7cebe366489e55e730983bf
Ming Hong <tangmh17@gmail.com>
2026-05-26 20:57:00 +0800

    Add frontend/src/components/RegisterForm.tsx

---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React, { useState } from 'react';
+import PasswordValidator from '@/components/PasswordValidator';
+
+export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState(false);
+    
+    const handleSignUp = async (e: React.BaseSyntheticEvent) => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            const res = await fetch('http://127.0.0.1:8000/api/auth/register')
+        }
+    }
+}
